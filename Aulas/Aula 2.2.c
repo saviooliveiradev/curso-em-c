9a797bade097a6af65fece037ad7e559f2dc40e3
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-05-02 21:56:36 -0300

    Aula 2.2.c
    Entrada  de Dadis

---
+++ Aulas/Aula 2.2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    
+    int idade = 0; //variável de número inteiro, que começa com o valor 0.
+
+    printf("O valor inicial da idade: %d.\n", idade);
+
+    printf("Digite sua idade:\n");
+    scanf("%d", &idade);
+
+    printf("Idade  informada: %d.\n", idade);
+
+}
