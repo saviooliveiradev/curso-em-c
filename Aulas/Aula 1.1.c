a998fc9d20c5ef6f471d6cd804214965d1293259
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-05-02 21:02:16 -0300

    Aula 1.1

---
+++ Aulas/Aula 1.1.c
@@ -0,0 +1,8 @@
+#include <stdio.h> //é uma biblioteca permite que envoque funcionalidades e funções para que o usuário entenda oq ta acontecendo com o programa
+#include <stdlib.h> //usando por conta do = system("pause");
+
+int main (){
+    printf("Hello, world!\n"); //sub comando que pula de linha 
+
+    system("pause"); //uma função para executar o programa no windowws, precionar uma tecla para continuar o programa 
+}
