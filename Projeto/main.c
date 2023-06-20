6177fc3251abe2cdb5f7ad4845ec777aaa7c94c0
saviooliveira_dev <saviooliveiratec@gmail.com>
2023-06-20 15:01:41 -0300

    Projetinho

---
+++ Projeto/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include "poligonos.h"
+
+int main(){
+
+    printf("%.2f.\n", calcAreaRetagulo(10, 10));
+    
+}
---
+++ Projeto/poligonos.h
@@ -0,0 +1,16 @@
+//declarando função a maquina.
+
+#define PI 3.141159265 //const PI
+
+//declarando função com parâmentro
+float calcPerimetroTriangulo(float a, float b, float c);
+
+float calcPerimetroRetangulo(float b, float a);
+
+float calcPerimetroCirculo(float r);
+
+float calcAreaTriangulo(float b, float a);
+
+float calcAreaRetagulo(float b, float a);
+
+float calcAreaCirculo(float r);
